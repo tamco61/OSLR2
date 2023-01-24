4c196048967c42e515bd892d93bf9918a555b95a
tamco61 <42232243+tamco61@users.noreply.github.com>
2023-01-24 17:04:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
-#include <string>
 #include <iostream>
-#include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 using namespace std;
 
+
+
 int main()
 {
+    char* filename;
+    cin >> filename;
     
-    char* c;
-    cin >> c;
-    cout << c;
+    int file = open(filename, O_RDONLY);
+    
+    char c;
+    string s = "";
+    float num;
+    float sum;
+    
 
-    const char* s = c;
-   	
-    
-    
+    while (read(file, &c, sizeof(c)))
+    {
+        if (c == ' ')
+        {
+        	num = stof(s);
+            sum = sum + num;
+            s = "";
+        
+        }
+        else if (c == 10)
+        {
+        	cout << sum << endl;
+        	sum = 0;
+        }
+        else
+        {
+        	s = s + c;
+        }
+    }
     
     return 0;
 }
