260ce22f8dbe2e8cc85e992cc611fb8a982da459
tamco61 <42232243+tamco61@users.noreply.github.com>
2023-03-26 11:43:55 +0300

    Update slave.cpp

--- slave.cpp
+++ slave.cpp
@@ -1,27 +1,54 @@
 #include <iostream>
-
-using namespace std;
+#include <unistd.h>
 
 int main()
 {
-	float num;
-	float sum = 0;
-	int n = 0;
 
-	while (cin >> num)
-	{
-		n += 1;
-		if ((n % 3) == 0)
-		{  
-			sum += num;
-			cout << sum << endl;
-			sum = 0;
-		}
+	int x = 0, res, n = 1;
+	float sum = 0;
+	char c;
+	int check = 0;
+	float k;
+
+	while (1) {
+    	res = read(STDIN_FILENO, &c, sizeof(char));
+		if (c == ' ' || c == '\n' || c == '\t') 
+		{
+			k = x;
+			k /= n;
+			sum += k;
+			x = 0;
+			n = 1;
+			check = 0;
+		} 
+		else if (c == '.')
+		{
+			check = 1;
+		}	
 		else
 		{
-			sum += num;
+			x = x*10 + c - '0';
+			if (check)
+			{
+				n *= 10;
+			}
+		}	
+
+		if (res == 0 || c == '\n') 
+		{
+			if (res == 0) 
+			{
+				if ((write(STDOUT_FILENO, "C", sizeof(char)) == -1)) return -51;
+				break;
+			}
+			if ((write(STDOUT_FILENO, &sum, sizeof(float)) == -1)) return -52;
+			x = 0;
+			sum = 0;
+			n = 1;
+			check = 0;
 		}
 	}
 
 
+	return EOF;
 }
